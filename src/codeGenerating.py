f4f1aa175dccf3e8090a68c5532c74a3f78ad269
Charles Koutcheme <charles.koutcheme@aalto.fi>
2023-07-27 17:40:55 +0300

    updated codeGenerating

--- src/codeGenerating.py
+++ src/codeGenerating.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, argparse
+import time
 import pandas as pd
 import numpy as np
 import html2text
@@ -77,6 +78,77 @@ def generateGPTAnswer(prompt, api_key=os.environ.get('OPEN_AI_KEY', None), model
     return gpt_code
 
 
+def get_results(problems_df, model):
+    problem_ids = []
+    prompts = []
+    codes = []
+
+    for i in tqdm(range(len(problems_df))):
+        row = problems_df.iloc[i]
+        problem_description = html2text.html2text(row['prompt'])
+        skeleton_code = row['skeleton']
+        prompt = generatePropmt(problem_description, skeleton_code)
+        try: 
+            code = extractResultCode(generateGPTAnswer(prompt, model=model))
+        except openai.error.ServiceUnavailableError:
+            print("Service unavailable, trying again")
+            continue
+
+        problem_ids.append(row['id'])
+        prompts.append(prompt)
+        codes.append(code)
+        
+    result_df = pd.DataFrame({
+        "problem_id":problem_ids, 
+        "prompts": prompts, 
+        "code": codes
+    })
+
+    return result_df
+
+
+def save_results(dataframe, args):
+    print(f'ensure that {args.output_dir} directory exists...')
+    os.makedirs(args.output_dir, exist_ok=True)
+    print(f'successfully ensured directory existence')
+
+    config_version = args.config.split("config/")[1].split(".json")[0]
+    filename = f"{args.model}_{config_version}_result.csv"
+    result_filename = os.path.join(args.output_dir, filename)
+    dataframe.to_csv(result_filename)
+
+
+def query_openai(problems_df, args):
+    dataframe = []
+    remaining, n_trials = len(problems_df),  3
+    while remaining > 0 and n_trials > 0:
+        result_df = get_results(problems_df, args.model)
+        remaining -= len(result_df)
+        dataframe.append(result_df)
+        problems_df = problems_df[~problems_df['id'].isin(result_df["problem_id"])]
+        print("Remaining", remaining, "n_trials", n_trials)
+        print("sleeping before trying again")
+        time.sleep(60)
+
+    print("Number of dataframes in pack", len(dataframe))
+    print(dataframe)
+    dataframe = pd.concat(dataframe, axis=0, ignore_index=True)
+    return dataframe
+
+
+def load_dataset(args):
+    problems_filename = os.path.join(args.input_dir, 'falconcode_v1_table_problems_updated.csv')
+    problems_df = pd.read_csv(problems_filename)
+    problems_df = problems_df.fillna("")
+    print("Original number of problems", len(problems_df))
+    problems_df = problems_df.drop_duplicates(subset='id', keep='first')
+    print("After dropping duplicates", len(problems_df))
+    problems_to_drop = json2data(args.validity)
+    problems_df = problems_df[~problems_df['id'].isin(problems_to_drop)]
+    print("after removing bad problems", len(problems_df))
+
+    return problems_df
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Call chatGPT-3.5 api to generate code and save into a csv file.')
     parser.add_argument('-m', '--model', help='the model used to generate the code', default='gpt-3.5-turbo')
@@ -85,54 +157,13 @@ def parse_args():
     parser.add_argument('-c', '--config', help='the configuration', default='config/v1.json')
     parser.add_argument('-v', '--validity', help='the valid ones', default='config/validity.json')
     
-
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    print(f'ensure that {args.output_dir} directory exists...')
-    os.makedirs(args.output_dir, exist_ok=True)
-    print(f'successfully ensured directory existence')
-
-    problems_filename = os.path.join(args.input_dir, 'falconcode_v1_table_problems_updated.csv')
-    problems_df = pd.read_csv(problems_filename)
-    problems_df = problems_df.drop_duplicates(subset='id', keep='first')
-    problems_df = problems_df.fillna("")
-    
-    # config = json2data(args.config)
-    problems_to_drop = json2data(args.validity)
-    
-    problems_df = problems_df[~problems_df['id'].isin(problems_to_drop)]
-    print(len(problems_df))
-    assert problems_df['id'].isin(problems_to_drop).sum() == 0
-
-    print("looping over the problems")
-    
-    """
-    looping over all problems
-    """
-    problem_ids = []
-    prompts = []
-    codes = []
-    for i in tqdm(range(len(problems_df))):
-        row = problems_df.iloc[i]
-        problem_description = html2text.html2text(row['prompt'])
-        skeleton_code = row['skeleton']
-        prompt = generatePropmt(problem_description, skeleton_code)
-        try: 
-            code = extractResultCode(generateGPTAnswer(prompt, model=args.model))
-        except openai.error.ServiceUnavailableError:
-            break
-        
-        problem_ids.append(row['id'])
-        prompts.append(prompt)
-        codes.append(code)
-        
-    result_df = pd.DataFrame({"problem_id":problem_ids, "prompts": prompts, "code": codes})
-    result_filename = os.path.join(args.output_dir, args.model+'_'+args.config.split("config/")[1].split(".json")[0]+'_result.csv')
-    result_df.to_csv(result_filename)
-
-    print('Wrote results into', result_df)
+    problems_df = load_dataset(args)
+    dataframe = query_openai(problems_df, args)
+    save_results(dataframe, args)
     
 
 if __name__ == '__main__':
